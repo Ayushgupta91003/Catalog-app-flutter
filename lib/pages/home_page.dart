af9365645e7e7aa513bdd1bad2c73317ff9adfd3
Ayush Gupta <ayush91003@gmail.com>
2024-01-11 15:24:54 +0530

    some minor changes. to be ignored

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,26 +7,20 @@ import '../widgets/item_widget.dart';
 // import 'package:google_fonts/google_fonts.dart';
 
 import 'package:velocity_x/velocity_x.dart';
-
 class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
-
 class _HomePageState extends State<HomePage> {
   // const HomePage({super.key});
   final int days = 30;
-
   final String name = "codepur";
-
-
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     loadData();
   }
-
   loadData() async{
     await Future.delayed(Duration(seconds: 2));
     final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
@@ -34,23 +28,18 @@ class _HomePageState extends State<HomePage> {
     final decodeData = jsonDecode(catalogJson);
     var productsData = decodeData["products"];
     // print(productsData);
-
     // List<Item> list = List.from(productsData).map((e) => null).toList();
     // List<Item> list = List.from(productsData.map((e) => Item.fromJson(e))).toList();
     // List<Item> list = List<Item>.from(productsData.map((e) => Item.fromMap(e))).toList();   // sir ka nahi chal raha tha.
     CatalogModel.items = List.from(productsData).map<Item>((item)=>Item.fromMap(item)).toList();
     setState(() {
       // data change ho raha hai. kyuki items mein ek hi item hai. isiliye setState call karna padta hai.
-
     });
   }
-
   // num can take both int and double.
   @override
   Widget build(BuildContext context) {
-
     // final dummyList = List.generate(50, (index)=>CatalogModel.items[0]);
-
     return Scaffold(
       appBar: AppBar(
 
@@ -63,12 +52,10 @@ class _HomePageState extends State<HomePage> {
             // ),
         ),
 
-
         // backgroundColor: Colors.white,
         // by default ye text ios me center aur android mein left side.
       ),
       body: Center(
-
         child: Container(
           color: Colors.white,
           child:(CatalogModel.items!=null && CatalogModel.items.isNotEmpty)?
